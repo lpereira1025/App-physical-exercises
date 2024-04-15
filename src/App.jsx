292b2b39650249a03e5a419c0c86cb7ecd22d713
Lucas Pereira <lpereira349@gmail.com>
2024-04-14 21:55:04 -0300

    adding content to files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/icons-material'
+
 import './App.css'
+import { NavBar } from './components/NavBar'
+import { Footer } from './components/Footer'
+import { Home } from './pages/Home'
+import { ExerciseDetail } from './pages/ExerciseDetail'
+
 
 function App() {
 
   return (
-    <>
-    </>
+    <Box width='25rem' sx={{width: {xl: '1488px'}}} m='auto'>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
+      </Routes>
+      <Footer/>
+    </Box>
   )
 }
 
